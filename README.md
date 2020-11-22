07376463d9e2d278770de5a1ebb8582967a7cd2a
sagheerahmadGmit <37177234+sagheerahmadGmit@users.noreply.github.com>
2020-11-22 15:13:01 +0000

    added web service skeleton

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # EmergingTechAssessment
+
+#### How to run the web service
+
+```bash
+$ export FLASK_APP=WebService.py
+$ flask run
+ * Running on http://127.0.0.1:5000/
+ ```
---
+++ WebService.py
@@ -0,0 +1,8 @@
+#Adapted from https://flask.palletsprojects.com/en/1.1.x/quickstart/
+
+from flask import Flask
+app = Flask(__name__)
+
+@app.route('/')
+def hello_world():
+    return 'Hello, World!'
