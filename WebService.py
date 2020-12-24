29b713a144188976f66afe279d581a1504069c35
sagheerahmadGmit <37177234+sagheerahmadGmit@users.noreply.github.com>
2020-12-24 14:56:04 +0000

    Building an API frontend

--- WebService.py
+++ WebService.py
@@ -1,8 +1,14 @@
 #Adapted from https://flask.palletsprojects.com/en/1.1.x/quickstart/
 
 from flask import Flask
+import numpy as np
+
 app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return 'Hello, World!'
+    return app.send_static_file('index.html')
+
+@app.route('/api/normal')
+def normal():
+  return {"value": np.random.normal()}
