7bc4fc20979c200e52f508990cb596f37b36645e
sagheerahmadGmit <37177234+sagheerahmadGmit@users.noreply.github.com>
2021-01-06 21:20:19 +0000

    Added css to the index page and added research into notebook

--- WebService.py
+++ WebService.py
@@ -4,35 +4,43 @@ import numpy as np
 import pandas as pd
 # Plotting
 import matplotlib.pyplot as plt
-# keras imports to split the dataset for training and testing 
+# keras imports to split the dataset for training and testing
 import tensorflow as tf
 # Neural networks.
 import tensorflow.keras as kr
 from tensorflow import keras
 from tensorflow.keras import layers
 from tensorflow.keras.layers import Dense
-from tensorflow.keras.optimizers import SGD,Adam
+from tensorflow.keras.optimizers import Adam
 from tensorflow.python.keras.models import Model
 from flask import Flask
 from flask import request
+import sys
 
 import json
 
 app = Flask(__name__)
 
+
 @app.route('/')
 def hello_world():
     return app.send_static_file('index.html')
 
-@app.route('/calculate/', methods=["POST"])
-def power():
+@app.route('/calculate/turbine/power', methods=['POST'])
+def parse_request():
+    data = request.data
+    jon = json.loads(data)
+    #get the speed input from the user
+    windSpeed = float(jon['speed'])
+    #load in the data from the model that was saved from the jupyter notebook
     model = tf.keras.models.load_model('SavedModelData.h5')
-    wSpeed = request.get_json()
-    prediction = model.predict(wSpeed)
-    #convert to a list and get the first index
-    prediction = json.loads(prediction)
-    prediction = [ prediction[0] ] 
-    return render_template('index.html', embed=prediction)
-    
+    #predict the power value using the model
+    prediction = model.predict([windSpeed])
+    #turn 2d array into 1d array for predictions
+    flatten = prediction.flatten()
+    print(flatten[0])
+    #return the value as a string
+    return str(flatten[0])
+
 if __name__ == '__main__':
     app.run(debug=True)
